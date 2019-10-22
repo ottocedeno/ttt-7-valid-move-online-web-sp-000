fdc42a63bcceda953783e436524a37f1683dd31b
Otto Cedeno <otto@ottostacos.com>
2019-10-22 18:00:48 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,7 +9,7 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
-  if board[index] = "X" || board[index] ="O"
+  if board[index] = "X" || board[index] = "O"
     true
   elsif board[index] = " "
     "Fuck you"
